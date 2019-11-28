46eb10082f46cd5006908199d645d05bb5bfdaa3
mariotinelli <49689215+mariotinelli@users.noreply.github.com>
2019-11-28 13:51:18 -0300

    Update 1248.py

--- 1248.py
+++ 1248.py
@@ -1,3 +1,17 @@
+def cria_lista(string):
+    nova_lista = []
+    for indice in string:
+        nova_lista.append(indice)
+    return nova_lista
+
+
+def cria_string(lista):
+    nova_string = ''
+    for i in lista:
+        nova_string += i
+    return nova_string
+
+
 n = int(input())
 
 for i in range(n):
@@ -9,20 +23,23 @@ for i in range(n):
     ingerido = ''
     jantar = ''
 
-    for indice in dieta:
-        lista_dieta.append(indice)
+    lista_dieta = cria_lista(dieta)
+    lista_dieta.sort()
+    dieta = cria_string(lista_dieta)
 
-    lista_dieta = lista_dieta.sort()
-
-
+    cheater = 0
 
     for indice in manha:
         for indice2 in lista_dieta:
-            if indice == indice2:
+            if indice not in lista_dieta:
+                cheater = 1
+            elif indice == indice2:
                 ingerido += indice
     for indice in almoco:
         for indice2 in lista_dieta:
-            if indice == indice2:
+            if indice not in lista_dieta:
+                cheater = 1
+            elif indice == indice2:
                 ingerido += indice
 
     for indice in lista_dieta:
@@ -31,11 +48,21 @@ for i in range(n):
                 if indice not in jantar:
                     jantar += indice
 
-    comparador = (jantar + manha + almoco) == dieta
+    lista_ingerido = cria_lista(ingerido)
+    lista_ingerido.sort()
+    ingerido = cria_string(lista_ingerido)
 
-    if not comparador:
-        print('CHEATER')
-    elif jantar == '':
-        print(*lista_dieta, sep='')
+    lista_jantar = cria_lista(jantar)
+    lista_jantar.sort()
+
+    if cheater == 0:
+        if jantar == '':
+            if ingerido == dieta:
+                print('')
+            else:
+                print(cria_string(lista_dieta).upper())
+        elif lista_jantar:
+            print(cria_string(lista_jantar).upper())
     else:
-        print(jantar)
+        print('CHEATER')
+
